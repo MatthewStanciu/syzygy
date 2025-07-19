38cad3f7ec12bca503f32d747e5c16f493a65115
Matthew Stanciu <mattbstanciu@gmail.com>
2025-07-19 15:35:25 -0700

    error handling + real prod url

--- clues/app/page.tsx
+++ clues/app/page.tsx
@@ -5,13 +5,17 @@ export default async function Home() {
   const redis = Redis.fromEnv();
   const baseUrl =
     process.env.NODE_ENV === "production"
-      ? "https://92e90cff4313.ngrok-free.app"
-      : "http://localhost:3001";
+      ? "https://sssyyyzzzyyygggyyy.vercel.app"
+      : "http://localhost:3000";
 
-  const phrases = await fetch(`${baseUrl}/api/phrases`, {
-    next: { revalidate: 30 },
-  }).then((r) => r.json());
-  console.log("phrases!", phrases);
+  try {
+    const phrases = await fetch(`${baseUrl}/api/phrases`, {
+      next: { revalidate: 30 },
+    }).then((r) => r.json());
 
-  return <List phrases={phrases} />;
+    console.log("phrases!", phrases);
+    return <List phrases={phrases} />;
+  } catch (err: any) {
+    return <p>fetch failed: {err.toString()}</p>;
+  }
 }
