1dd33d455d27fd4d5a612a0c5342654ecaea9b19
Matthew Stanciu <mattbstanciu@gmail.com>
2025-07-20 10:26:45 -0700

    back to 1000

--- intercom/src/index.ts
+++ intercom/src/index.ts
@@ -122,7 +122,7 @@ app.post("/intercom", async (request, _res) => {
             await openDoor(callControlId);
             // await markPhraseAsUsed(matchingPhrase.key);
             // await resetPhrasesIfAllUsed();
-          }, 750);
+          }, 1000);
         }
       }
     } else if (call.data.event_type === "call.dtmf.received") {
